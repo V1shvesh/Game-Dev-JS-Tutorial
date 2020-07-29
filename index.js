979fc6603385a140cec7ca537718149db4dbc949
V1shvesh <vishvesh.ext@grofers.com>
2020-07-30 01:07:23 +0530

    Level 3.2: User Input

--- index.js
+++ index.js
@@ -6,17 +6,38 @@ let CANVAS_WIDTH = 400;
 let CANVAS_HEIGHT = 400;
 
 function updateCanvasDimensions() {
-    CANVAS_WIDTH = window.innerWidth;
-    CANVAS_HEIGHT = window.innerHeight;
-    canvas.width = CANVAS_WIDTH;
-    canvas.height = CANVAS_HEIGHT;
-  }
-  
+  CANVAS_WIDTH = window.innerWidth;
+  CANVAS_HEIGHT = window.innerHeight;
+  canvas.width = CANVAS_WIDTH;
+  canvas.height = CANVAS_HEIGHT;
+}
+
 updateCanvasDimensions();
+
+// Cursor Coordinates
+let mouseX = CANVAS_WIDTH / 2;
+let mouseY = CANVAS_HEIGHT / 2;
+
+// Mouse Move Event Handler
+window.addEventListener('mousemove', event => {
+  mouseX = event.x;
+  mouseY = event.y;
+});
+
+const debounce = (func) => {
+  let timer;
+  return (event) => {
+    if (timer) { clearTimeout(timer) }
+    timer = setTimeout(func, 100, event);
+  }
+}
+
+// Window Resize Event Handler
+window.addEventListener('resize', debounce(updateCanvasDimensions));
 
 const ctx = canvas.getContext('2d');
 
-// Circle class
+
 class Circle {
   constructor(x, y, radius, fillColor) {
     this.x = x;
@@ -75,22 +96,38 @@ class Asteroid extends Circle {
   }
 }
 
+// Warp Drive Sold Separately
+class Spaceship extends Circle {
+  constructor() {
+    super(mouseX, mouseY, 20, '#1A535C');
+  }
+
+  // Follow the cursor
+  update() {
+    this.x = mouseX;
+    this.y = mouseY;
+  }
+}
+
 // Game Params
 const NO_OF_ASTEROIDS = 5
 
 let asteroidGang = Array(NO_OF_ASTEROIDS).fill(null);
-
+let spaceship = null;
 
 function init() {
   asteroidGang = asteroidGang.map(() => new Asteroid());
+  spaceship = new Spaceship();
 }
 
 function updateState() {
   asteroidGang.forEach(asteroid => asteroid.update());
+  spaceship.update();
 }
 
 function drawFrame() {
   asteroidGang.forEach(asteroid => asteroid.draw());
+  spaceship.draw()
 }
 
 function loop() {
