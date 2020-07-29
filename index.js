dbb2977547272caea50c547315da478919317859
V1shvesh <vishvesh.ext@grofers.com>
2020-07-29 23:44:45 +0530

    Level 1.2: Setup

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import './styles.css';
 
-// 2. Query the DOM Tree for its DOM node.
 const canvas = document.querySelector('canvas');
 
 let CANVAS_WIDTH = 400;
@@ -15,5 +14,32 @@ function updateCanvasDimensions() {
   
 updateCanvasDimensions();
 
-// 4. Fetch the rendering context for programmatically drawing on the canvas.
 const ctx = canvas.getContext('2d');
+
+// Style Attributes
+ctx.fillStyle = 'black';
+ctx.strokeStyle = 'black';
+
+// Rectangles
+ctx.fillRect(25, 25, 100, 100);
+ctx.clearRect(45, 45, 60, 60);
+ctx.strokeRect(50, 50, 50, 50);
+
+// Triangle
+ctx.beginPath();
+ctx.moveTo(200, 100);
+ctx.lineTo(260, 40);
+ctx.lineTo(320, 100);
+ctx.closePath()
+ctx.fill();
+// Or to draw just the outline
+// ctx.stroke()
+
+// Circle
+ctx.beginPath()
+// ctx.arc(75, 200, 50, 0, Math.PI*2, false);
+// Or a half-circle
+// ctx.arc(75, 200, 50, 0, Math.PI, true);
+// Let's turn that frown upside down
+ctx.arc(75, 200, 50, 0, Math.PI, false);
+ctx.stroke();
